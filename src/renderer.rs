abf644a903ed74f41c318cf245bca4e1b4a93519
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2019-11-14 12:48:41 -0800

    Lightmaps don't seem to be very good, reverting.

--- src/renderer.rs
+++ src/renderer.rs
@@ -11,19 +11,12 @@ use vulkano::pipeline::ComputePipeline;
 
 use std::sync::Arc;
 
-use crate::shaders::{
-    self, AssignLightmapsShaderLayout, BasicRaytraceShaderLayout, CameraVectorPushConstants,
-    FinalizeShaderLayout, UpdateLightmapsShaderLayout, UpdateLightmapsPushData
-};
+use crate::shaders::{ self, BasicRaytraceShaderLayout, CameraVectorPushConstants, };
 use crate::util;
 
 type WorldData = CpuAccessibleBuffer<[u16]>;
 type WorldImage = StorageImage<Format>;
-type LatResetBuffer = CpuAccessibleBuffer<[u32]>;
 type BasicRaytracePipeline = ComputePipeline<PipelineLayout<BasicRaytraceShaderLayout>>;
-type AssignLightmapsPipeline = ComputePipeline<PipelineLayout<AssignLightmapsShaderLayout>>;
-type UpdateLightmapsPipeline = ComputePipeline<PipelineLayout<UpdateLightmapsShaderLayout>>;
-type FinalizePipeline = ComputePipeline<PipelineLayout<FinalizeShaderLayout>>;
 
 type GenericImage = StorageImage<Format>;
 type GenericDescriptorSet = dyn DescriptorSet + Sync + Send;
@@ -31,31 +24,6 @@ type GenericDescriptorSet = dyn DescriptorSet + Sync + Send;
 const WORLD_SIZE: usize = 256;
 const L2_STEP: usize = 16;
 const L2_SIZE: usize = WORLD_SIZE / L2_STEP;
-
-const LIGHTMAP_SIZE: u32 = L2_STEP as u32; // One lightmap covers LIGHTMAP_SIZE^3 voxels.
-
-const LIGHTMAP_RES_L0: u32 = 8; // Pixels per voxel.
-const LIGHTMAP_QUANTITY_L0: u32 = 32; // Number of lightmaps to create.
-const LIGHTMAP_ATLAS_WIDTH_L0: u32 = 8; // Number of lightmaps to pack along the x axis of atlas.
-                                        // Height is calculated from QUANTITY / WIDTH.
-
-const LIGHTMAP_RES_L1: u32 = 4;
-const LIGHTMAP_QUANTITY_L1: u32 = 128;
-const LIGHTMAP_ATLAS_WIDTH_L1: u32 = 16;
-
-const LIGHTMAP_RES_L2: u32 = 2;
-const LIGHTMAP_QUANTITY_L2: u32 = 512;
-const LIGHTMAP_ATLAS_WIDTH_L2: u32 = 32;
-
-// Calculations show each lightmap atlas contains 24 million pixels. Assuming 32 bits per pixel,
-// this makes all atlases consume 288MB.
-
-// Enough to store LIGHTMAP_QUANTITY_L2 + 2 extra bytes, rounded up.
-const LIGHTMAP_TABLE_SIZE: u32 = 1024;
-// Value 0: lightmap is used? value 1-3: xyz of region where used.
-const LIGHTMAP_DESCRIPTION_WIDTH: u32 = 16;
-// Enough to hold indexes of all lightmaps + 1 extra value, rounded up.
-const LIGHTMAP_UPDATE_QUEUE_LENGTH: u32 = 2048; 
 
 // Positive Y (angle PI / 2) is forward
 // Positive X is to the right
@@ -78,27 +46,8 @@ pub struct Renderer {
     world_l2_data: Arc<WorldData>,
     world_l2_image: Arc<WorldImage>,
 
-    lightmap_assignment_buffer: Arc<GenericImage>,
-    lightmap_availability_table: Arc<GenericImage>,
-    lat_reset_buffer: Arc<LatResetBuffer>,
-    lightmap_operation_buffer: Arc<GenericImage>,
-    lightmap_table: Arc<GenericImage>,
-    lightmap_usage_buffer: Arc<GenericImage>,
-    lightmap_update_queue: Arc<GenericImage>,
-    update_lightmaps_push_data: UpdateLightmapsPushData,
-
-    lightmap_atlas_l0: Arc<GenericImage>,
-    lightmap_atlas_l1: Arc<GenericImage>,
-    lightmap_atlas_l2: Arc<GenericImage>,
-
     basic_raytrace_pipeline: Arc<BasicRaytracePipeline>,
     basic_raytrace_descriptors: Arc<GenericDescriptorSet>,
-    assign_lightmaps_pipeline: Arc<AssignLightmapsPipeline>,
-    assign_lightmaps_descriptors: Arc<GenericDescriptorSet>,
-    update_lightmaps_pipeline: Arc<UpdateLightmapsPipeline>,
-    update_lightmaps_descriptors: Arc<GenericDescriptorSet>,
-    finalize_pipeline: Arc<FinalizePipeline>,
-    finalize_descriptors: Arc<GenericDescriptorSet>,
 }
 
 struct RenderBuilder {
@@ -213,65 +162,6 @@ impl RenderBuilder {
         (world_l1_data, world_l1_image, world_l2_data, world_l2_image)
     }
 
-    fn make_lightmap_atlas(
-        &self,
-        pixels_per_voxel: u32,
-        quantity: u32,
-        atlas_width: u32,
-    ) -> Arc<GenericImage> {
-        let lightmap_resolution = pixels_per_voxel * LIGHTMAP_SIZE;
-        // *3 because we need seperate sets of layers for x, y, and z axes.
-        let layers_per_lightmap = LIGHTMAP_SIZE * 3;
-        let atlas = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim3d {
-                width: lightmap_resolution * atlas_width,
-                height: lightmap_resolution * (quantity / atlas_width),
-                depth: layers_per_lightmap,
-            },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        atlas
-    }
-
-    fn build_lat(&self) -> (Arc<GenericImage>, Arc<LatResetBuffer>) {
-        let lightmap_availability_table = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim2d {
-                width: LIGHTMAP_TABLE_SIZE,
-                height: 3,
-            },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let mut reset_data = Vec::new();
-        for lightmap_resolution in 0..3 {
-            let atlas_size = match lightmap_resolution {
-                0 => LIGHTMAP_QUANTITY_L0,
-                1 => LIGHTMAP_QUANTITY_L1,
-                2 => LIGHTMAP_QUANTITY_L2,
-                _ => unreachable!(),
-            };
-            for index in 0..LIGHTMAP_TABLE_SIZE - 2 {
-                reset_data.push(index);
-            }
-            // Elements 0..LIGHTMAP_AVAILABILITY_TABLE_SIZE represent indexes of available
-            // lightmaps of whatever our current resolution is.
-            reset_data.push(0);
-            reset_data.push(atlas_size);
-        }
-        let lat_reset_buffer = CpuAccessibleBuffer::from_iter(
-            self.device.clone(),
-            BufferUsage::all(),
-            reset_data.into_iter(),
-        )
-        .unwrap();
-        (lightmap_availability_table, lat_reset_buffer)
-    }
-
     fn build(self) -> Renderer {
         let (target_width, target_height) = match self.target_image.dimensions() {
             Dimensions::Dim2d { width, height } => (width, height),
@@ -280,90 +170,7 @@ impl RenderBuilder {
 
         let (world_l1_data, world_l1_image, world_l2_data, world_l2_image) = self.make_world();
 
-        let position_buffer = StorageImage::new(
-            self.device.clone(),
-            self.target_image.dimensions(),
-            Format::R16G16B16A16Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let hit_result_buffer = StorageImage::new(
-            self.device.clone(),
-            self.target_image.dimensions(),
-            Format::R16Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-
-        let lightmap_assignment_buffer = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim3d {
-                width: L2_SIZE as u32,
-                height: L2_SIZE as u32,
-                depth: L2_SIZE as u32,
-            },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let (lightmap_availability_table, lat_reset_buffer) = self.build_lat();
-        let lightmap_operation_buffer = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim3d {
-                width: L2_SIZE as u32,
-                height: L2_SIZE as u32,
-                depth: L2_SIZE as u32,
-            },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let lightmap_table = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim3d {
-                width: LIGHTMAP_TABLE_SIZE,
-                height: LIGHTMAP_DESCRIPTION_WIDTH,
-                depth: 4u32, // Only going to use 3 but powers of two.
-            },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let lightmap_usage_buffer = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim1d { width: 16u32 },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-        let lightmap_update_queue = StorageImage::new(
-            self.device.clone(),
-            Dimensions::Dim1d { width: LIGHTMAP_UPDATE_QUEUE_LENGTH },
-            Format::R32Uint,
-            Some(self.queue.family()),
-        )
-        .unwrap();
-
-        let lightmap_atlas_l0 = self.make_lightmap_atlas(
-            LIGHTMAP_RES_L0,
-            LIGHTMAP_QUANTITY_L0,
-            LIGHTMAP_ATLAS_WIDTH_L0,
-        );
-        let lightmap_atlas_l1 = self.make_lightmap_atlas(
-            LIGHTMAP_RES_L1,
-            LIGHTMAP_QUANTITY_L1,
-            LIGHTMAP_ATLAS_WIDTH_L1,
-        );
-        let lightmap_atlas_l2 = self.make_lightmap_atlas(
-            LIGHTMAP_RES_L2,
-            LIGHTMAP_QUANTITY_L2,
-            LIGHTMAP_ATLAS_WIDTH_L2,
-        );
-
         let basic_raytrace_shader = shaders::load_basic_raytrace_shader(self.device.clone());
-        let assign_lightmaps_shader = shaders::load_assign_lightmaps_shader(self.device.clone());
-        let update_lightmaps_shader = shaders::load_update_lightmaps_shader(self.device.clone());
-        let finalize_shader = shaders::load_finalize_shader(self.device.clone());
 
         let basic_raytrace_pipeline = Arc::new(
             ComputePipeline::new(
@@ -379,101 +186,12 @@ impl RenderBuilder {
                 .unwrap()
                 .add_image(world_l2_image.clone())
                 .unwrap()
-                .add_image(position_buffer.clone())
-                .unwrap()
-                .add_image(hit_result_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_operation_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_usage_buffer.clone())
-                .unwrap()
-                .build()
-                .unwrap(),
-        );
-
-        let assign_lightmaps_pipeline = Arc::new(
-            ComputePipeline::new(
-                self.device.clone(),
-                &assign_lightmaps_shader.main_entry_point(),
-                &(),
-            )
-            .unwrap(),
-        );
-        let assign_lightmaps_descriptors: Arc<dyn DescriptorSet + Sync + Send> = Arc::new(
-            PersistentDescriptorSet::start(assign_lightmaps_pipeline.clone(), 0)
-                .add_image(lightmap_operation_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_availability_table.clone())
-                .unwrap()
-                .add_image(lightmap_assignment_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_table.clone())
-                .unwrap()
-                .add_image(lightmap_update_queue.clone())
-                .unwrap()
-                .build()
-                .unwrap(),
-        );
-
-        let update_lightmaps_pipeline = Arc::new(
-            ComputePipeline::new(
-                self.device.clone(),
-                &update_lightmaps_shader.main_entry_point(),
-                &(),
-            )
-            .unwrap(),
-        );
-        let update_lightmaps_descriptors: Arc<dyn DescriptorSet + Sync + Send> = Arc::new(
-            PersistentDescriptorSet::start(update_lightmaps_pipeline.clone(), 0)
-                .add_image(world_l1_image.clone())
-                .unwrap()
-                .add_image(world_l2_image.clone())
-                .unwrap()
-                .add_image(lightmap_table.clone())
-                .unwrap()
-                .add_image(lightmap_update_queue.clone())
-                .unwrap()
-                .add_image(lightmap_atlas_l0.clone())
-                .unwrap()
-                .add_image(lightmap_atlas_l1.clone())
-                .unwrap()
-                .add_image(lightmap_atlas_l2.clone())
-                .unwrap()
-                .build()
-                .unwrap(),
-        );
-
-        let finalize_pipeline = Arc::new(
-            ComputePipeline::new(
-                self.device.clone(),
-                &finalize_shader.main_entry_point(),
-                &(),
-            )
-            .unwrap(),
-        );
-        let finalize_descriptors: Arc<dyn DescriptorSet + Sync + Send> = Arc::new(
-            PersistentDescriptorSet::start(finalize_pipeline.clone(), 0)
-                .add_image(position_buffer.clone())
-                .unwrap()
-                .add_image(hit_result_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_assignment_buffer.clone())
-                .unwrap()
-                .add_image(lightmap_table.clone())
-                .unwrap()
-                .add_image(lightmap_usage_buffer.clone())
-                .unwrap()
                 .add_image(self.target_image.clone())
                 .unwrap()
-                .add_image(lightmap_atlas_l0.clone())
-                .unwrap()
-                .add_image(lightmap_atlas_l1.clone())
-                .unwrap()
-                .add_image(lightmap_atlas_l2.clone())
-                .unwrap()
                 .build()
                 .unwrap(),
         );
+
 
         Renderer {
             target_width,
@@ -485,29 +203,8 @@ impl RenderBuilder {
             world_l2_data,
             world_l2_image,
 
-            lightmap_assignment_buffer,
-            lightmap_availability_table,
-            lat_reset_buffer,
-            lightmap_operation_buffer,
-            lightmap_table,
-            lightmap_usage_buffer,
-            lightmap_update_queue,
-            update_lightmaps_push_data: UpdateLightmapsPushData {
-                random_seed: 0,
-            },
-
-            lightmap_atlas_l0,
-            lightmap_atlas_l1,
-            lightmap_atlas_l2,
-
             basic_raytrace_pipeline,
             basic_raytrace_descriptors,
-            assign_lightmaps_pipeline,
-            assign_lightmaps_descriptors,
-            update_lightmaps_pipeline,
-            update_lightmaps_descriptors,
-            finalize_pipeline,
-            finalize_descriptors,
         }
     }
 }
@@ -539,26 +236,10 @@ impl Renderer {
                 .copy_buffer_to_image(self.world_l1_data.clone(), self.world_l1_image.clone())
                 .unwrap()
                 .copy_buffer_to_image(self.world_l2_data.clone(), self.world_l2_image.clone())
-                .unwrap()
-                .clear_color_image(self.lightmap_assignment_buffer.clone(), [0x3000u32].into())
-                .unwrap()
-                .copy_buffer_to_image(
-                    self.lat_reset_buffer.clone(),
-                    self.lightmap_availability_table.clone(),
-                )
-                .unwrap()
-                .clear_color_image(self.lightmap_table.clone(), [0x0u32].into())
                 .unwrap();
             self.image_update_requested = false;
         }
-        self.update_lightmaps_push_data.random_seed = (self.update_lightmaps_push_data.random_seed + 1) % 4096;
         add_to
-            .clear_color_image(self.lightmap_operation_buffer.clone(), [3u32].into())
-            .unwrap()
-            .clear_color_image(self.lightmap_usage_buffer.clone(), [0u32].into())
-            .unwrap()
-            .clear_color_image(self.lightmap_update_queue.clone(), [0u32].into())
-            .unwrap()
             // Do initial raytrace to determine which voxels are on screen.
             .dispatch(
                 [self.target_width / 8, self.target_height / 8, 1],
@@ -573,28 +254,6 @@ impl Renderer {
                     right: [right.x * 0.3, right.y * 0.3, right.z * 0.3],
                     up: [up.x * 0.3, up.y * 0.3, up.z * 0.3],
                 },
-            )
-            .unwrap()
-            .dispatch(
-                [L2_SIZE as u32 / 4, L2_SIZE as u32 / 4, L2_SIZE as u32 / 4],
-                self.assign_lightmaps_pipeline.clone(),
-                self.assign_lightmaps_descriptors.clone(),
-                (),
-            )
-            .unwrap()
-            .dispatch(
-                [LIGHTMAP_UPDATE_QUEUE_LENGTH / 64, 1, 1],
-                self.update_lightmaps_pipeline.clone(),
-                self.update_lightmaps_descriptors.clone(),
-                self.update_lightmaps_push_data.clone(),
-            )
-            .unwrap()
-            // Combine computed data into final image.
-            .dispatch(
-                [self.target_width / 8, self.target_height / 8, 1],
-                self.finalize_pipeline.clone(),
-                self.finalize_descriptors.clone(),
-                (),
             )
             .unwrap()
     }
--- src/shaders.rs
+++ src/shaders.rs
@@ -4,22 +4,10 @@ use std::sync::Arc;
 
 // Unfortunately the shader! macro does not trigger a recompile whenever source code changes.
 fn _watchdog() {
-    let _source = include_bytes!("../shaders/assign_lightmaps.comp");
     let _source = include_bytes!("../shaders/basic_raytrace.comp");
-    let _source = include_bytes!("../shaders/finalize.comp");
     let _source = include_bytes!("../shaders/screen.vert");
     let _source = include_bytes!("../shaders/screen.frag");
-    let _source = include_bytes!("../shaders/update_lightmaps.comp");
 }
-
-mod assign_lightmaps {
-    vulkano_shaders::shader! {
-        ty: "compute",
-        path: "shaders/assign_lightmaps.comp"
-    }
-}
-pub use assign_lightmaps::Layout as AssignLightmapsShaderLayout;
-pub use assign_lightmaps::Shader as AssignLightmapsShader;
 
 mod basic_raytrace {
     vulkano_shaders::shader! {
@@ -30,15 +18,6 @@ mod basic_raytrace {
 pub use basic_raytrace::ty::CameraVectors as CameraVectorPushConstants;
 pub use basic_raytrace::Layout as BasicRaytraceShaderLayout;
 pub use basic_raytrace::Shader as BasicRaytraceShader;
-
-mod finalize {
-    vulkano_shaders::shader! {
-        ty: "compute",
-        path: "shaders/finalize.comp"
-    }
-}
-pub use finalize::Layout as FinalizeShaderLayout;
-pub use finalize::Shader as FinalizeShader;
 
 mod screen_vs {
     vulkano_shaders::shader! {
@@ -58,26 +37,8 @@ mod screen_fs {
 pub use screen_vs::Layout as ScreenVertexShaderLayout;
 pub use screen_vs::Shader as ScreenVertexShader;
 
-mod update_lightmaps {
-    vulkano_shaders::shader! {
-        ty: "compute",
-        path: "shaders/update_lightmaps.comp"
-    }
-}
-pub use update_lightmaps::ty::PushData as UpdateLightmapsPushData;
-pub use update_lightmaps::Layout as UpdateLightmapsShaderLayout;
-pub use update_lightmaps::Shader as UpdateLightmapsShader;
-
-pub fn load_assign_lightmaps_shader(device: Arc<Device>) -> AssignLightmapsShader {
-    assign_lightmaps::Shader::load(device).unwrap()
-}
-
 pub fn load_basic_raytrace_shader(device: Arc<Device>) -> BasicRaytraceShader {
     basic_raytrace::Shader::load(device).unwrap()
-}
-
-pub fn load_finalize_shader(device: Arc<Device>) -> FinalizeShader {
-    finalize::Shader::load(device).unwrap()
 }
 
 pub fn load_screen_vertex_shader(device: Arc<Device>) -> ScreenVertexShader {
@@ -86,8 +47,4 @@ pub fn load_screen_vertex_shader(device: Arc<Device>) -> ScreenVertexShader {
 
 pub fn load_screen_fragment_shader(device: Arc<Device>) -> ScreenFragmentShader {
     screen_fs::Shader::load(device).unwrap()
-}
-
-pub fn load_update_lightmaps_shader(device: Arc<Device>) -> UpdateLightmapsShader {
-    update_lightmaps::Shader::load(device).unwrap()
 }
