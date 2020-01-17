3f40d391244ad64f91d0c124898304ad4cd801c5
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-01-16 18:07:04 -0800

    Added controls back.

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -35,26 +35,69 @@ impl Game {
     }
 
     pub fn new() -> Game {
+        let args: Vec<_> = env::args().collect();
         let mut result = Game {
             camera: Camera::new(),
             world: world::make_world(),
             controls: Self::make_controls(),
             sun_angle: 0.0,
         };
-        result.camera.origin.x = 446.0;
-        result.camera.origin.y = 283.0;
-        result.camera.origin.z = 39.0;
-        result.camera.heading.0 = -5.26;
-        result.camera.pitch.0 = -0.20;
-        result.sun_angle = 1.3;
+        if args.len() > 1 {
+            result.camera.origin.x = args[1].parse().unwrap();
+            result.camera.origin.y = args[2].parse().unwrap();
+            result.camera.origin.z = args[3].parse().unwrap();
+            result.camera.heading.0 = args[4].parse().unwrap();
+            result.camera.pitch.0 = args[5].parse().unwrap();
+            result.sun_angle = args[6].parse().unwrap();
+        } else {
+            result.camera.origin.x = 40.0;
+            result.camera.origin.y = 40.0;
+            result.camera.origin.z = 80.0;
+        }
         result
     }
 
     // Called after all controls have been updated.
     pub fn tick(&mut self, dt: f32) {
+        if self.controls.is_held("sunup") {
+            self.sun_angle += dt * 1.0;
+        } else if self.controls.is_held("sundown") {
+            self.sun_angle -= dt * 1.0;
+        }
+
+        let dx: f32 = if self.controls.is_held("left") {
+            -1.0
+        } else if self.controls.is_held("right") {
+            1.0
+        } else {
+            0.0
+        };
+        let dy: f32 = if self.controls.is_held("backward") {
+            -1.0
+        } else if self.controls.is_held("forward") {
+            1.0
+        } else {
+            0.0
+        };
+        let dz: f32 = if self.controls.is_held("down") {
+            -1.0
+        } else if self.controls.is_held("up") {
+            1.0
+        } else {
+            0.0
+        };
+        let amount = dt * 50.0;
+        let util::TripleEulerVector { forward, up, right } =
+            util::compute_triple_euler_vector(self.camera.heading, self.camera.pitch);
+        let forward = forward.normalize();
+        let up = up.normalize();
+        let right = right.normalize();
+        self.camera.origin += amount * forward * dy + amount * up * dz + amount * right * dx;
     }
 
     pub fn on_mouse_move(&mut self, x: f64, y: f64) {
+        self.camera.heading.0 = (-x / 80.0) as f32;
+        self.camera.pitch.0 = ((256.0 - y) / 200.0) as f32;
     }
 
     pub fn borrow_world(&self) -> &World {
