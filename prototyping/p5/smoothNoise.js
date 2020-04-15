738413320dfd484f78557c4faf1f67227e329e90
joshua-maros <60271685+joshua-maros@users.noreply.github.com>
2020-04-14 19:22:49 -0700

    Cool

--- prototyping/p5/smoothNoise.js
+++ prototyping/p5/smoothNoise.js
@@ -1,68 +1,38 @@
 function setup() {
     createCanvas(400, 400);
+    noiseSeed(0);
     noLoop();
 }
 
-function intHash(a)
-{
-   a = (a+0x7ed55d16) + (a<<12);
-   a = (a^0xc761c23c) ^ (a>>19);
-   a = (a+0x165667b1) + (a<<5);
-   a = (a+0xd3a2646c) ^ (a<<9);
-   a = (a+0xfd7046c5) + (a<<3);
-   a = (a^0xb55a4f09) ^ (a>>16);
-   return a;
+const d = 0.1;
+function noisedx(x, y) {
+    return (noise(x + d, y) - noise(x - d, y)) / (d * 2);
 }
 
-function randomValueAtPoint(x, y, seed) {
-    if (!seed) seed = 0;
-    [x, y] = [Math.round(x), Math.round(y)];
-    randomSeed(intHash(x) ^ intHash(~y) ^ intHash(seed));
-    return random();
+function noisedy(x, y) {
+    return (noise(x, y + d) - noise(x, y - d)) / (d * 2);
 }
 
-function coherentNoise(x, y, seed) {
-    const [tl, tr, bl, br] = [
-        randomValueAtPoint(Math.floor(x), Math.floor(y), seed),
-        randomValueAtPoint(Math.ceil(x), Math.floor(y), seed),
-        randomValueAtPoint(Math.floor(x), Math.ceil(y), seed),
-        randomValueAtPoint(Math.ceil(x), Math.ceil(y), seed),
-    ];
-    const ltr = x % 1.0;
-    const ttb = y % 1.0;
-    const [top, bottom] = [lerp(tl, tr, ltr), lerp(bl, br, ltr)];
-    return lerp(top, bottom, ttb);
-}
-
-function perlin(x, y, layers, seed) {
-    let size = 1.0, power = 0.5;
-    let value = coherentNoise(x, y, seed) * power;
-    let offset = 500;
-    for ([lsize, lpower] of layers) {
-        size *= lsize;
-        power *= lpower;
-        value += coherentNoise((x + offset) * size, y * size, seed) * power;
-        offset += 100;
-    }
-    return value;
-}
 
 function func(x, y) {
-    const perlinLayers = [
-        [2, 0.5],
-        [2, 0.5],
-        [2, 0.5],
-        [2, 0.5],
-    ];
-    const intensity = perlin(x, y, perlinLayers);
-    return [intensity * 255, intensity * 255, intensity * 255];
+    const height = noise(x, y);
+    const dx = noisedx(x, y);
+    const dy = noisedy(x, y);
+    const slope = Math.hypot(dx, dy);
+
+    function signed(value) { return (value + 1) * 128; }
+    function unsigned(value) { return value * 255; }
+    const r = unsigned(height);
+    const g = unsigned(slope);
+    const b = 0;
+    return [r, g, b];
 }
 
 function draw() {
     background(0);
     loadPixels();
-    for (let x = 0; x < 200; x++) {
-        for (let y = 0; y < 200; y++) {
+    for (let x = 0; x < 400; x++) {
+        for (let y = 0; y < 400; y++) {
             const pindex = (y * 400 + x) * 4;
             const scale = 30;
             const value = func(x / scale, y / scale);
